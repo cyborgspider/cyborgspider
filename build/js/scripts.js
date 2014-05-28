c5d132ef257a1220b6ce0b50fffc2c0892ae2a18
Carlos Ortega <exoticorion@gmail.com>
2014-05-27 17:50:03 -0700

    a lot of work on portfolio

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -1,1 +1,73 @@
-$(function(){var a,b,c;return $(".visit").attr("target","_blank"),c=function(a){var b;return a.preventDefault(),b=$(this).data("filter"),$(this).siblings().removeClass("active"),$(this).addClass("active"),$("#work-list li").filter(b).show(),$("#work-list li").not(b).hide()},$("#filter-nav").on("click","a",c),b=$("#work-detail").find("article:first-child"),b.addClass("active"),a=function(){return console.log("derp"),b=$("#work-detail").find("article.active")},$("#next-project").click(function(c){return c.preventDefault(),a(),b.removeClass("active"),0===b.next().length?(b=$("#work-detail").find("article:first-child"),b.addClass("active")):b.next().addClass("active")}),$("#prev-project").click(function(c){return c.preventDefault(),a(),b.removeClass("active"),0===b.prev().length?(b=$("#work-detail").find("article:last-child"),b.addClass("active")):b.prev().addClass("active")})});
+var loadHash;
+
+loadHash = function() {
+  var currentSection, hashLocation;
+  hashLocation = window.location.hash.substr(1);
+  if (hashLocation === '') {
+    currentSection = $('#work-detail').find('article:first-child');
+    return currentSection.addClass('active');
+  } else {
+    currentSection = $('#work-detail').find('article[data-article=' + hashLocation + ']');
+    currentSection.siblings().removeClass('active');
+    return currentSection.addClass('active');
+  }
+};
+
+$(function() {
+  var filterMe, hashChange, projectNavigator;
+  loadHash();
+  $('.visit').attr('target', '_blank');
+  filterMe = function(e) {
+    var whichFilter;
+    e.preventDefault();
+    whichFilter = $(this).data('filter');
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    $('#work-list li').filter(whichFilter).show();
+    return $('#work-list li').not(whichFilter).hide();
+  };
+  $('#filter-nav').on('click', 'a', filterMe);
+  hashChange = function(section) {
+    window.location.hash = section;
+    return window.scrollTo(0, 0);
+  };
+  projectNavigator = function(direction) {
+    var currentSection, nextHash, nextSection, prevHash, prevSection;
+    currentSection = $('#work-detail').find('article.active');
+    nextSection = currentSection.next();
+    prevSection = currentSection.prev();
+    nextHash = nextSection.data('article');
+    prevHash = prevSection.data('article');
+    currentSection.removeClass('active');
+    if (direction === 'next') {
+      if (currentSection.next().length === 0) {
+        nextSection = $('#work-detail').find('article:first-child');
+        nextHash = nextSection.data('article');
+        nextSection.addClass('active');
+        hashChange(nextHash);
+      } else {
+        nextSection.addClass('active');
+        hashChange(nextHash);
+      }
+    }
+    if (direction === 'prev') {
+      if (currentSection.prev().length === 0) {
+        prevSection = $('#work-detail').find('article:last-child');
+        prevSection.addClass('active');
+        prevHash = prevSection.data('article');
+        return hashChange(prevHash);
+      } else {
+        prevSection.addClass('active');
+        return hashChange(prevHash);
+      }
+    }
+  };
+  $('#next-project').click(function(e) {
+    e.preventDefault();
+    return projectNavigator('next');
+  });
+  return $('#prev-project').click(function(e) {
+    e.preventDefault();
+    return projectNavigator('prev');
+  });
+});
--- site/scripts/scripts.coffee
+++ site/scripts/scripts.coffee
@@ -1,4 +1,16 @@
+loadHash = ->
+  hashLocation = window.location.hash.substr(1)
+  if hashLocation is ''
+    currentSection = $('#work-detail').find('article:first-child')
+    currentSection.addClass('active')
+  else
+    currentSection = $('#work-detail').find('article[data-article='+hashLocation+']')
+    currentSection.siblings().removeClass('active')
+    currentSection.addClass('active')
+
 $ ->
+  loadHash()
+
   $('.visit').attr('target','_blank')
 
   filterMe = (e) ->
@@ -11,28 +23,45 @@ $ ->
 
   $('#filter-nav').on 'click', 'a', filterMe
 
-  currentSection = $('#work-detail').find('article:first-child')
-  currentSection.addClass('active')
+  hashChange = (section) ->
+    window.location.hash = section
+    window.scrollTo(0,0)
 
-  assignCurrentArticle = ->
+  projectNavigator = (direction) ->
     currentSection = $('#work-detail').find('article.active')
+    nextSection = currentSection.next()
+    prevSection = currentSection.prev()
+    nextHash = nextSection.data('article')
+    prevHash = prevSection.data('article')
+    currentSection.removeClass('active')
+
+    if direction is 'next'
+      #when you've reached the end of the line, go back to the beginning article
+      if currentSection.next().length == 0
+        nextSection = $('#work-detail').find('article:first-child')
+        nextHash = nextSection.data('article')
+        nextSection.addClass('active')
+        hashChange(nextHash)
+      else
+        nextSection.addClass('active')
+        hashChange(nextHash)
+
+    if direction is 'prev'
+      #when you're at the beginning of the line, and a user presses Previous, go backwards
+      if currentSection.prev().length == 0
+        prevSection = $('#work-detail').find('article:last-child')
+        prevSection.addClass('active')
+        prevHash = prevSection.data('article')
+        hashChange(prevHash)
+      #otherwise, go in Previous order as expected
+      else
+        prevSection.addClass('active')
+        hashChange(prevHash)
 
   $('#next-project').click (e) ->
     e.preventDefault()
-    assignCurrentArticle()
-    currentSection.removeClass('active')
-    if currentSection.next().length == 0
-      currentSection = $('#work-detail').find('article:first-child')
-      currentSection.addClass('active')
-    else
-      currentSection.next().addClass('active')
+    projectNavigator('next')
 
   $('#prev-project').click (e) ->
     e.preventDefault()
-    assignCurrentArticle()
-    currentSection.removeClass('active')
-    if currentSection.prev().length == 0
-      currentSection = $('#work-detail').find('article:last-child')
-      currentSection.addClass('active')
-    else
-      currentSection.prev().addClass('active')
+    projectNavigator('prev')
